91d5add2d97818e8db32e4625c17b92f4c64c093
Himanshu <himanshushettykt03@gmail.com>
2026-08-02 17:15:55 +0530

    fix(header): set manual scroll restoration on reload to prevent navbar masking

--- src/hooks/UseSticky.ts
+++ src/hooks/UseSticky.ts
@@ -9,7 +9,7 @@ const UseSticky = (): StickyState => {
    const [sticky, setSticky] = useState(false);
 
    const stickyHeader = (): void => {
-      if (window.scrollY > 200) {
+      if (window.scrollY > 30) {
          setSticky(true);
       } else {
          setSticky(false);
@@ -17,15 +17,26 @@ const UseSticky = (): StickyState => {
    };
 
    useEffect(() => {
-      window.addEventListener("scroll", stickyHeader);
+      if (typeof window !== "undefined") {
+         if ("scrollRestoration" in window.history) {
+            window.history.scrollRestoration = "manual";
+         }
+         if (window.scrollY < 200) {
+            window.scrollTo(0, 0);
+         }
+      }
+
+      stickyHeader();
+      window.addEventListener("scroll", stickyHeader, { passive: true });
 
       return (): void => {
          window.removeEventListener("scroll", stickyHeader);
       };
    }, []);
+
    return {
       sticky,
    };
 }
 
-export default UseSticky
+export default UseSticky;
